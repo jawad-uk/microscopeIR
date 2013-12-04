0b20ee12797af987e86e1fb83a791c4c7576ea75
Jawad Abdulsamad <jhsamad@gmail.com>
2013-12-04 16:57:04 +0300

    added animation

--- collections/posts.js
+++ collections/posts.js
@@ -5,6 +5,12 @@ Posts.allow({
   remove: ownsDocument
 });
 
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // may only edit the following two fields:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
 
 Meteor.methods({
   post: function(postAttributes) {
@@ -30,11 +36,27 @@ Meteor.methods({
     var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
       userId: user._id, 
       author: user.username, 
-      submitted: new Date().getTime()
+      submitted: new Date().getTime(),
+      commentsCount: 0,
+      upvoters: [], votes: 0
     });
     
     var postId = Posts.insert(post);
     
     return postId;
+  },
+  
+  upvote: function(postId) {
+    var user = Meteor.user();
+    // ensure the user is logged in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to upvote");
+    Posts.update({
+      _id: postId, 
+      upvoters: {$ne: user._id}
+    }, {
+      $addToSet: {upvoters: user._id},
+      $inc: {votes: 1}
+    });
   }
 });
